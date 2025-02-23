ac563f77cf9eb35fe394843a49d60db27cc43ea9
VINAYAKASAI-6701 <vinaysai6701@gmail.com>
2025-02-23 23:14:24 +0530

    Added stockbutselldp.py

---
+++ stockbutselldp.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+        dp=[[0]*2 for _ in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            dp[ind][1]=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
+            dp[ind][0]=max(prices[ind]+dp[ind+1][1],dp[ind+1][0])
+        return dp[0][1]
