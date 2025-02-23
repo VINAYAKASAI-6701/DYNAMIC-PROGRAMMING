797b60657c112a3be8e241b91a232829a5c5a8b4
VINAYAKASAI-6701 <vinaysai6701@gmail.com>
2025-02-23 23:14:24 +0530

    Added stockiiidp.py

---
+++ stockiiidp.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def maxProfit(prices: List[int]) -> int:
+    n = len(prices)
+    dp = [[[0] * 3 for _ in range(2)] for _ in range(n + 1)]
+
+    for ind in range(n - 1, -1, -1):
+        for buy in range(2):
+            for cap in range(1, 3):
+                if buy:
+                    dp[ind][buy][cap] = max(-prices[ind] + dp[ind + 1][0][cap], dp[ind + 1][1][cap])
+                else:
+                    dp[ind][buy][cap] = max(prices[ind] + dp[ind + 1][1][cap - 1], dp[ind + 1][0][cap])
+
+    return dp[0][1][2]
